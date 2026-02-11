90f5b47a6e083b629a49b77542d722f7a3df5fda
Brylinito <bchavezc@educantabria.es>
2026-02-11 22:35:30 +0100

    Proyecto mouse-learn
    Aprendo un poco el uso de useEffect()

--- mouse-learn/src/App.jsx
+++ mouse-learn/src/App.jsx
@@ -4,21 +4,42 @@ import "./App.css";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0}); 
   useEffect(() => {
     console.log("efecto", { enabled });
 
     const handleMove = (event) => {
       const { clientX, clientY } = event;
       console.log("hadleMove", { clientX, clientY });
+      setPosition({x: clientX, y:clientY});
     };
 
-    window.addEventListener("pointermove", handleMove);
+    if(enabled){
+      window.addEventListener('pointermove', handleMove);
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    }//los use effect devuelven, cuando se destruye el componente, o cambie la dependencia. 
+    //Empezamos desde nuveo. ¿¿Cuando se ejecuta esto= 
+
+    
   }, [enabled]);
 
   return (
     <>
       <main>
-        <h1>Mouse</h1>
+        <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%', 
+          opacity: 0.8,
+          pointerEvents: 'none', 
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}/>
         <button onClick={() => setEnabled(!enabled)}>
           {enabled ? "Desactivar" : "Activar"} : Buton
         </button>
